7aedcb8a2b27a4278d17f6b7099ee85e3997e649
SamIfesi <Ekesamrose@gmail.com>
2026-04-27 16:16:06 +0100

    fix: correct parameter name in getEvents method for API call

--- src/services/events.service.js
+++ src/services/events.service.js
@@ -2,7 +2,7 @@ import api from './api'
 
 const EventsService = {
   async getEvents(param = {}) {
-    const response = await api.get('/events', { param });
+    const response = await api.get('/events', { params });
     return response.data.data; // returns {events: [], pagination: {}}
   },
 
